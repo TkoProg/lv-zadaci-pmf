a605e0b524d972d10280c6ebbe64790fab50e1e9
tamiroladejo <koltam09@gmail.com>
2023-10-21 22:56:52 +0200

    Prvi zadatak i pdf za trecu sedmicu.

---
+++ Sedmica 3/Zadaci/Zadatak 2.py
@@ -0,0 +1,11 @@
+def trocifren(br):
+    if 100 <= br <= 999:
+        br = str(br)
+        return f"Proizvod: {int(br[0])*int(br[1])*int(br[2])}"
+    else:
+        return "Nije trocifren!"
+
+
+broj = int(input("Unesite trocifren broj, program ispisuje proizvod cifara tog broja: "))
+
+print(trocifren(broj))
