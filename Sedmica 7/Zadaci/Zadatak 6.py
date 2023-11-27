cb15b74bea6a4eceb4136a115ba5fd20ced57520
tamiroladejo <koltam09@gmail.com>
2023-11-27 12:49:18 +0100

    Sesti zadatak za sedmu sedmicu.

---
+++ Sedmica 7/Zadaci/Zadatak 6.py
@@ -0,0 +1,26 @@
+import math
+
+
+def jelProst(n):
+    for i in range(2, math.floor(math.sqrt(n))+1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def najmanjiZjednicki(m,n):
+    najm = min(m, n)
+    nzd = 0
+    for i in range(1, najm+1):
+        if jelProst(i):
+            if m % i == 0 and n % i == 0:
+                nzd = i
+        else:
+            continue
+    return nzd
+
+
+m = int(input("Unesite prvi prirodan broj: "))
+n = int(input("Unesite drugi prirodan broj: "))
+
+print(f"Najveci prosti broj koji djeli brojeve {m} i {n} je: {najmanjiZjednicki(m,n)}")
