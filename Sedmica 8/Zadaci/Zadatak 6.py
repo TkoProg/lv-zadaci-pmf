561d40985a5b3fbbe270b7f09008e54ac62d3dc6
tamiroladejo <koltam09@gmail.com>
2023-12-01 12:03:43 +0100

    Sesti zadatak za osmu sedmicu.

--- Sedmica 8/Zadaci/Zadatak 6.py
+++ Sedmica 8/Zadaci/Zadatak 6.py
@@ -11,4 +11,4 @@ lista.sort(reverse=True)
 maks_element = lista[0]
 n_ti_najveci = lista[n-1]
 
-print(f"Razlika izmedju {najveceg} i {n-n_ti_najveci} je {maks_element - n_ti_najveci}")
+print(f"Razlika izmedju {maks_element} i {n-n_ti_najveci} je {maks_element - n_ti_najveci}")
